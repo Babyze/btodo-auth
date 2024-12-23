52741234b4da45f627ccaed9dcd4f0f147516bdb
Babyze <danghuynhanh1999@gmail.com>
2024-12-23 23:18:27 +0700

    Commit JWT, JWT strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,27 @@ import { AuthEntity } from './entities/auth.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AuthRepository } from './auth.repository';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { Jwt } from 'src/common/jwt/jwt';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule, DatabaseModule.forFeature([AuthEntity])],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([AuthEntity]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET_KEY'),
+        signOptions: {
+          expiresIn: configService.get('JWT_EXPIRE'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, AuthRepository],
+  providers: [AuthService, AuthRepository, JwtStrategy, Jwt],
 })
 export class AuthModule {}
