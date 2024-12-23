b86daa772ac3bb85a8c5cd70c2979e0b222dabe5
Babyze <danghuynhanh1999@gmail.com>
2024-12-23 20:11:11 +0700

    Commit setup microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import { protobufPackage } from './auth/pb/auth.pb';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: protobufPackage,
+      protoPath: join(__dirname, '..', config.get('GRPC_PROTO_PATH')),
+      url: `${config.get('GRPC_HOST')}:${config.get('GRPC_PORT')}`,
+    },
+  });
+
+  await app.startAllMicroservices();
 }
 bootstrap();
